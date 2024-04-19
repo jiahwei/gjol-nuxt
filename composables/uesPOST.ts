4042f7608dae4ed1bfe796d8de4e58f89c05b9e4
净北 <j.moring@foxmail.com>
2024-04-19 17:32:26 +0800

    usePOST 类型注释及调整

--- composables/uesPOST.ts
+++ composables/uesPOST.ts
@@ -4,13 +4,23 @@ type Options<T> = Omit<UseFetchOptions<T>, 'default'> & { default: () => T | Ref
 type body<T> = UseFetchOptions<T>['body'];
 type Url = string | (() => string);
 type defaultValue<T> = () => T | Ref<T>;
+type OtherConfig<T> = Omit<UseFetchOptions<T>, 'default'>;
 
-export function usePOST<T>(url: Url, body: body<T>, defaultValue: defaultValue<T>) {
+/**
+ * 发起POST请求
+ * @param url API路径
+ * @param body 请求体
+ * @param defaultValue 默认值
+ * @param [otherConfig={}] 其他 UseFetchOptions 的参数
+ * @returns useFetch 方法包装后的返回值
+ */
+export function usePOST<T>(url: Url, body: body<T>, defaultValue: defaultValue<T>, otherConfig: OtherConfig<T> = {}) {
   const options: Options<T> = {
     method: 'POST',
     body,
     key: toValue(url),
-    default: defaultValue
+    default: defaultValue,
+    ...otherConfig
   };
   return useAPI(url, options);
 }
