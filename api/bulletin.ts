5cc2493f3671272b8f2bbddf565c24d1c64b1332
jhw-cn <j.moring@foxmail.com>
2025-04-06 22:20:15 +0800

    refactor(api): 重构公告API并添加类型定义
    重构公告API以使用自动生成的类型定义，增加代码的可维护性和类型安全性。同时，更新了相关文件和配置以支持新的API结构。

--- api/bulletin.ts
+++ api/bulletin.ts
@@ -1,34 +1,42 @@
 import { useGet } from '@/composables/useGet'
+import type { components } from '@/api-types/api'
 
 const baseUrl = `/api`
-interface ContentTotalArr {
-  name: string
-  leng: number
-}
 
-interface GetNewBulletin {
-  date: Date | undefined
-  totalLen: number
-  orderId: number
-  contentTotalArr: ContentTotalArr[]
-  name: string
-  releaseID: number
-  reselseName: string
-  order: number
-}
+// 定义类型别名，使代码更简洁
+type BulletinInfo = components['schemas']['BulletinInfo']
+type BulletinDB = components['schemas']['BulletinDB']
+type ListInVersionReturn = components['schemas']['ListInVersionReturn']
+// type DatePayload = components['schemas']['DatePayload']
 
-const defaultGetNewBulletin: GetNewBulletin = {
-  date: undefined,
+// 默认值
+const defaultGetNewBulletin: BulletinInfo = {
+  date: '',
+  orderId: 0,
   totalLen: 0,
-  contentTotalArr: [],
-  name: '',
-  releaseID: 0,
-  reselseName: '',
+  id: null,
+  name: null,
   order: 0,
+  type: 'routine',
 }
 
 export default {
   getNewBulletin: () => {
-    return useGet<GetNewBulletin>(`${baseUrl}/bulletins/new`, () => defaultGetNewBulletin)
+    return useGet<BulletinInfo>(`${baseUrl}/bulletins/new`, () => defaultGetNewBulletin)
+  },
+
+  getBulletinById: (bulletinId: number) => {
+    return useGet<BulletinDB>(`${baseUrl}/bulletins/query?bulletin_id=${bulletinId}`, () => ({
+      bulletin_date: '',
+      original_date: '',
+      total_leng: 0,
+      content_total_arr: '',
+      version_id: 0,
+      type: 'routine',
+    }))
+  },
+
+  getListInVersion: () => {
+    return useGet<ListInVersionReturn[]>(`${baseUrl}/bulletins/listInVersion`, () => [])
   },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app-home" class="text-font-primary transition-colors">
+    {{ newBulletinInfo }}
   </div>
 </template>
 
