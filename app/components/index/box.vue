dc280452837eb58f9c8f162548ed935e73cfdd11
净北 <j.moring@foxmail.com>
2025-09-10 23:42:47 +0800

    feat: 添加首页组件及交叉观察器功能
    添加交叉观察器用于检测元素进入视图

--- app/components/index/box.vue
+++ app/components/index/box.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    
+  <div class="h-screen w-full  flex justify-center items-center b-red b-1px">
+    <div class="bg-green h-100px w-100px flex justify-center items-center text-gray-700" id="boxcontent">
+      111
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, onMounted } from 'vue'
 
 </script>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,45 @@
 <template>
   <div class="flex flex-col  justify-center items-center">
-    <!-- <version /> -->
-     <box v-for="value in [1, 2, 3]" :key="value" id="box-{{ value }}" />
+    <box v-for="(value, index) in [1, 2, 3]" :key="value" :id="`box-${value}`" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import version from '~/components/index/version.vue';
 import box from '~/components/index/box.vue';
-</script>
 
+
+function setIntersectionObserver() {
+  const { ssrContext } = useNuxtApp()
+  if (ssrContext) return;
+
+  const options = {
+    root: null,
+    threshold: 0.4,
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+  for (let i = 1; i <= 3; i++) {
+    const el = document.getElementById(`box-${i}`);
+    if (el) {
+      observer.observe(el);
+    }
+  }
+
+  function callback(entries: IntersectionObserverEntry[]) {
+    console.log(entries);
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log('进入视图', entry.target.id);
+        observer.unobserve(entry.target)
+      } else {
+        console.log('未进入视图', entry.target.id);
+      }
+    });
+  }
+}
+
+onMounted(() => {
+  setIntersectionObserver();
+})
+</script>
